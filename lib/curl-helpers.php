bdd151ba287a88fd6a68a47aae42938edc58f28f
Oscar Leche <oscar.leche@gmail.com>
2015-09-06 12:17:50 -0600

    initial commit

---
+++ lib/curl-helpers.php
@@ -0,0 +1,75 @@
+<?php
+	class CurlHelpers{
+		private $key;
+		private $secret;
+		
+		public function __construct($_key, $_secret){
+			$this->key = $_key;
+			$this->secret = $_secret;
+		}
+		
+		public function doCurlAuth($url, $params = array()){
+			//url-ify the data for the POST
+			$fields_string = "";
+			foreach($params as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+			rtrim($fields_string, '&');
+			
+			$ch = curl_init();
+			
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+			curl_setopt($ch, CURLOPT_USERPWD, $this->key.":".$this->secret);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+			curl_setopt($ch, CURLOPT_UNRESTRICTED_AUTH, 1);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // required as godaddy fails
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // required as godaddy fails
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT,1);
+			curl_setopt($ch, CURLOPT_POST, count($params));
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+			if(defined('CURLOPT_IPRESOLVE') && defined('CURL_IPRESOLVE_V4'))
+			{
+		    curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+			}
+			
+			$output = curl_exec($ch);
+			if(curl_errno($ch)){
+			    echo 'Curl error: ' . curl_error($ch);
+			}
+			print_r($output);
+			curl_close($ch);
+		
+			return json_decode($output);
+		}
+		
+		public function doGetCurlBearerAuth($url, $access_token){
+			$ch = curl_init();
+			$headers = array('Authorization: Bearer ' . $access_token);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+			curl_setopt($ch, CURLOPT_UNRESTRICTED_AUTH, 1);
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT,1);
+            curl_setopt($ch, CURLOPT_HTTPGET,1);
+			if(defined('CURLOPT_IPRESOLVE') && defined('CURL_IPRESOLVE_V4'))
+			{
+		    curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+			}
+		
+			$output = curl_exec($ch);
+			curl_close($ch);
+		
+			return json_decode($output);
+		}
+		
+		public function setKey($_key){
+			$this->key = $_key;
+		}
+		
+		public function setSecret($_secret){
+			$this->secret = $_secret;
+		}
+	}
+?>
---
+++ post/login.php
@@ -0,0 +1,42 @@
+<?php
+	include_once '../lib/curl-helpers.php';
+
+	$user = "13a3b20fa41aa80636cffe064abd3e07";
+	$pass = "ac2097a20d8b989d1b4305bfdc170b4d";
+	$authURL = "http://api.quejamatic.com/v1/session";
+	
+	$curlH = new CurlHelpers($user, $pass);
+	
+	$fields = array(
+		'username' => $_GET['username'],
+		'password' => $_GET['password'],
+		'scopes' => 'site-visitor'
+	);
+	
+	$response = $curlH->doCurlAuth($authURL, $fields);
+
+	if ($response->code == 200){
+
+		$response = $curlH->doGetCurlBearerAuth("http://api.quejamatic.com/v1/user/".$_GET['username']."/api", $response->access_token);
+		
+		if ($response->code == 200){
+			$curlH->setKey($response->credenciales->client_id);
+			$curlH->setSecret($response->credenciales->client_secret);
+			
+			$fields = array(
+				'scopes' => 'administrator'
+			);
+			
+			$response = $curlH->doCurlAuth($authURL, $fields);
+			
+			if ($response->code == 200){
+				session_start();
+				$_SESSION['token'] = $response->access_token;
+				$_SESSION['username'] = $_GET['username'];
+			}		
+		}
+	}
+    echo str_replace('\/','/',json_encode($response));
+	//echo json_encode($response,JSON_UNESCAPED_SLASHES);
+
+?>
