7c0e3e2717170bc15a18640d57752ca3db144a91
Oscar Leche <oscar.leche@gmail.com>
2015-09-07 11:13:42 -0600

    login test

--- post/login.php
+++ post/login.php
@@ -4,36 +4,36 @@
 	$user = "13a3b20fa41aa80636cffe064abd3e07";
 	$pass = "ac2097a20d8b989d1b4305bfdc170b4d";
 	$authURL = "http://api.quejamatic.com/v1/session";
-	
+
 	$curlH = new CurlHelpers($user, $pass);
-	
+
 	$fields = array(
 		'username' => $_GET['username'],
 		'password' => $_GET['password'],
-		'scopes' => 'site-visitor'
+		'scopes' => 'login'
 	);
-	
+
 	$response = $curlH->doCurlAuth($authURL, $fields);
 
 	if ($response->code == 200){
 
 		$response = $curlH->doGetCurlBearerAuth("http://api.quejamatic.com/v1/user/".$_GET['username']."/api", $response->access_token);
-		
+
 		if ($response->code == 200){
 			$curlH->setKey($response->credenciales->client_id);
 			$curlH->setSecret($response->credenciales->client_secret);
-			
+
 			$fields = array(
 				'scopes' => 'administrator'
 			);
-			
+
 			$response = $curlH->doCurlAuth($authURL, $fields);
-			
+
 			if ($response->code == 200){
 				session_start();
 				$_SESSION['token'] = $response->access_token;
 				$_SESSION['username'] = $_GET['username'];
-			}		
+			}
 		}
 	}
     echo str_replace('\/','/',json_encode($response));
